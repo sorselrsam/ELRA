722ad1ada68f78b9fedc83ec7f25078fa6b548f3
sorselrsam <109242292+sorselrsam@users.noreply.github.com>
2022-09-01 03:23:55 +0200

    Add files via upload

--- Config.py
+++ Config.py
@@ -1,13 +1,13 @@
 import os
 
 class Config(object):
-    API_ID = int(os.environ.get("APP_ID", "6435225"))
-    API_HASH = os.environ.get("API_HASH", "4e984ea35f854762dcde906dce426c2d")
+    API_ID = int(os.environ.get("APP_ID", "18196466"))
+    API_HASH = os.environ.get("API_HASH", "b85f12702596a6cfb6fb51b1c4e01630")
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
     STRING_SESSION = os.environ.get("STRING_SESSION", "")
     HEROKU_MODE = os.environ.get("HEROKU_MODE", None)
-    BOT_USERNAME = os.environ.get("BOT_USERNAME", "JMTHON_ROBOT")
-    SUPPORT = os.environ.get("SUPPORT", "JMTHON_SUPPORT")
-    CHANNEL = os.environ.get("CHANNEL", "JMTHON")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2ad68bd0e391a69163d0a.jpg")
+    BOT_USERNAME = os.environ.get("BOT_USERNAME", "Elrasam11111bot")
+    SUPPORT = os.environ.get("SUPPORT", "EL_RASA")
+    CHANNEL = os.environ.get("CHANNEL", "E_L_R_A_S_A_M")
+    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/2fbfedf9ae631c9c591ae.jpg")
     CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
